cb51fddb8ce9add499983019cb9ef96c4f88827a
Jhonatan <inge5neuromedia@gmail.com>
2020-07-08 12:40:40 -0500

    Cambiando ruta de imagenes para producción

--- src/app/vista-previa/vista-previa.component.ts
+++ src/app/vista-previa/vista-previa.component.ts
@@ -45,51 +45,51 @@ export class VistaPreviaComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // this.galleryImages = [
-    //   {
-    //     small: "/N-1008/assets/img/productos/producto-interna.png",
-    //     medium: "/N-1008/assets/img/productos/producto-interna.png",
-    //     big: "/N-1008/assets/img/productos/producto-interna.png"
-    //   },
-    //   {
-    //     small: "/N-1008/assets/img/productos/producto-interna.png",
-    //     medium: "/N-1008/assets/img/productos/producto-interna.png",
-    //     big: "/N-1008/assets/img/productos/producto-interna.png"
-    //   },
-    //   {
-    //     small: "/N-1008/assets/img/productos/producto-interna.png",
-    //     medium: "/N-1008/assets/img/productos/producto-interna.png",
-    //     big: "/N-1008/assets/img/productos/producto-interna.png"
-    //   },
-    //   {
-    //     small: "/N-1008/assets/img/productos/producto-interna.png",
-    //     medium: "/N-1008/assets/img/productos/producto-interna.png",
-    //     big: "/N-1008/assets/img/productos/producto-interna.png"
-    //   }
-    // ];
-
     this.galleryImages = [
       {
-        small: "/assets/img/productos/producto-interna.png",
-        medium: "/assets/img/productos/producto-interna.png",
-        big: "/assets/img/productos/producto-interna.png"
+        small: "/N-1008/assets/img/productos/producto-interna.png",
+        medium: "/N-1008/assets/img/productos/producto-interna.png",
+        big: "/N-1008/assets/img/productos/producto-interna.png"
       },
       {
-        small: "/assets/img/productos/producto-interna.png",
-        medium: "/assets/img/productos/producto-interna.png",
-        big: "/assets/img/productos/producto-interna.png"
+        small: "/N-1008/assets/img/productos/producto-interna.png",
+        medium: "/N-1008/assets/img/productos/producto-interna.png",
+        big: "/N-1008/assets/img/productos/producto-interna.png"
       },
       {
-        small: "/assets/img/productos/producto-interna.png",
-        medium: "/assets/img/productos/producto-interna.png",
-        big: "/assets/img/productos/producto-interna.png"
+        small: "/N-1008/assets/img/productos/producto-interna.png",
+        medium: "/N-1008/assets/img/productos/producto-interna.png",
+        big: "/N-1008/assets/img/productos/producto-interna.png"
       },
       {
-        small: "/assets/img/productos/producto-interna.png",
-        medium: "/assets/img/productos/producto-interna.png",
-        big: "/assets/img/productos/producto-interna.png"
+        small: "/N-1008/assets/img/productos/producto-interna.png",
+        medium: "/N-1008/assets/img/productos/producto-interna.png",
+        big: "/N-1008/assets/img/productos/producto-interna.png"
       }
     ];
+
+    // this.galleryImages = [
+    //   {
+    //     small: "/assets/img/productos/producto-interna.png",
+    //     medium: "/assets/img/productos/producto-interna.png",
+    //     big: "/assets/img/productos/producto-interna.png"
+    //   },
+    //   {
+    //     small: "/assets/img/productos/producto-interna.png",
+    //     medium: "/assets/img/productos/producto-interna.png",
+    //     big: "/assets/img/productos/producto-interna.png"
+    //   },
+    //   {
+    //     small: "/assets/img/productos/producto-interna.png",
+    //     medium: "/assets/img/productos/producto-interna.png",
+    //     big: "/assets/img/productos/producto-interna.png"
+    //   },
+    //   {
+    //     small: "/assets/img/productos/producto-interna.png",
+    //     medium: "/assets/img/productos/producto-interna.png",
+    //     big: "/assets/img/productos/producto-interna.png"
+    //   }
+    // ];
 
     this.carouselDescatadosUno = [
       {"img": 'assets/img/zapatillas/uno.jpg',"descuento":false},
